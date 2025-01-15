c883ac8857110f5d95f474778fb35cf578276f48
way911010 <69240308+way911010@users.noreply.github.com>
2025-01-15 17:30:59 +0800

    Add files via upload

--- js/rwd.js
+++ js/rwd.js
@@ -1,6 +1,6 @@
 function updateHeight() {
-    var newWidth = window.innerWidth-200;
-    var newHeight = window.innerHeight-200;
+    var newWidth = window.innerWidth-140;
+    var newHeight = window.innerHeight-150;
     var langflowChat = document.querySelector('langflow-chat');
     langflowChat.setAttribute('width', newWidth);
     langflowChat.setAttribute('height', newHeight);
